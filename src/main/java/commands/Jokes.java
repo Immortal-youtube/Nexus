d2daa0397539d8af7dbdcc488359ae507f4257be
Immortal-youtube <84240738+Immortal-youtube@users.noreply.github.com>
2022-08-19 10:16:25 +0530

    l

--- src/main/java/commands/Jokes.java
+++ src/main/java/commands/Jokes.java
@@ -7,17 +7,24 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class Jokes extends ListenerAdapter {
     String str;
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if(event.getName().equals("joke")){
-            String fin = JokesConnector.joke();
+            String joke = null;
+            try {
+                joke = JokesConnector.joke();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
 
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setTitle("Joke");
-            embedBuilder.addField(fin, "", false);
+            embedBuilder.addField(joke, "", true);
             embedBuilder.setColor(Color.GREEN);
             embedBuilder.setThumbnail("https://media.gq.com/photos/5720e7496a2af99a11a1dc8a/16:9/w_2560%2Cc_limit/Obama-Laugh.jpg");
             event.deferReply().queue();
--- src/main/java/commands/MongoDB/JokesConnector.java
+++ src/main/java/commands/MongoDB/JokesConnector.java
@@ -1,30 +1,40 @@
 package commands.MongoDB;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Random;
 
 public class JokesConnector {
 
-    public static String joke() {
-        Random random = new Random();
-        MongoClientURI clientURI = new MongoClientURI(System.getenv("MONGO_URL"));
-        MongoClient mongoClient = new MongoClient(clientURI);
-        MongoDatabase database = mongoClient.getDatabase("Score");
-        MongoCollection collection = database.getCollection("jokes");
-        Document document = (Document) collection.find(new Document("id", random.nextInt(250))).first();
-        String joke = document.getString("Jokes");
-        String fin = joke.replace("<>","\n");
-
-        return fin;
-
-
-
+    public static String joke() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(System.getenv("JOKE_LINK")))
+                .header(System.getenv("JOKE_API_HEADING"), System.getenv("JOKE_API"))
+                .header(System.getenv("JOKE_HOST"), System.getenv("JOKE_HOST_KEY"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
+        JsonArray array = jsonObject.get("body").getAsJsonArray();
+        System.out.println(array.get(0));
+        String setup = array.get(0).getAsJsonObject().get("setup").getAsString();
+        String punchline = array.get(0).getAsJsonObject().get("punchline").getAsString();
+        System.out.println(setup);
+        System.out.println(punchline);
+        return setup + "\n" + punchline;
     }
 
 }
